79510f92ecf13e1d831566ee0e86169b51e91229
prinxe2312 <princebansal2312@gmail.com>
2026-06-22 11:25:05 +0530

    Fix API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,24 +1,24 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
     'Content-Type': 'application/json'
   }
 });
 
-// Request interceptor to automatically add the authentication token
+// Add JWT token automatically
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
+
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
+
     return config;
   },
-  (error) => {
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
